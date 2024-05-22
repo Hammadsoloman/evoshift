49f99d3e29ee98b3e1f4289e3788b16e174efaf2
Salim Merazga <merazgasalim@hotmail.fr>
2024-05-22 21:28:32 +0100

    fix deployement bugs

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-
-
 // module.exports = {
 //   webpack: (config, {
 //       buildId,
@@ -28,49 +26,39 @@
 //   },
 // }
 
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['i.imgur.com', 'cdn.sanity.io'],
+    domains: ["i.imgur.com", "cdn.sanity.io"],
   },
   experimental: {
     appDir: true,
+    missingSuspenseWithCSRBailout: false,
   },
 
-  webpack: (config, {
-          buildId,
-          dev,
-          isServer,
-          defaultLoaders,
-          webpack
-      }) => {
-          // Note: we provide webpack above so you should not `require` it
-          // Perform customizations to webpack config
-          config.plugins.push(
-              new webpack.ProvidePlugin({
-                  $: "jquery",
-                  jQuery: "jquery",
-                  "window.jQuery": "jquery"
-              })
-          );
-          // Important: return the modified config
-          return config;
-      } ,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery",
+      })
+    );
+    // Important: return the modified config
+    return config;
+  },
 
-
-      typescript: {
-        // !! WARN !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
-        // !! WARN !!
-        ignoreBuildErrors: true,
-      },
-  
-
-
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
