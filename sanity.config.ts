a46a93c7fc5e76915f0387b2b4811ffebc5441c1
Acauthorized <acaauthorized@gmail.com>
2024-07-09 23:25:21 -0400

    update sanity confiq

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,59 +1,27 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
+'use client'
+
+/**
+ * This configuration is used to for the Sanity Studio that’s mounted on the `\app\studio\[[...tool]]\page.tsx` route
+ */
 
 import {visionTool} from '@sanity/vision'
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
 
-import {schemaTypes} from './schemas'
-import { myTheme } from './theme';
-import StudioNavbar from './components/StudioNavbar';
-import { iconPicker } from "sanity-plugin-icon-picker";
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
-import { structureTool } from 'sanity/structure'
-import { media } from 'sanity-plugin-media'
-import {  mediaAssetSource } from 'sanity-plugin-media'
-import { vercelDeployTool } from 'sanity-plugin-vercel-deploy'
- import { inlineSvgInput } from '@focus-reactive/sanity-plugin-inline-svg-input'
-
-
+// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
+import {apiVersion, dataset, projectId} from './sanity/env'
+import {schema} from './sanity/schema'
 
 export default defineConfig({
-  name: 'default',
-  title: 'Admin dashboard',
   basePath: '/studio',
-
-   projectId,
-  dataset ,
-
-  
+  projectId,
+  dataset,
+  // Add and edit the content schema in the './sanity/schema' folder
+  schema,
   plugins: [
-
-
-  
-
-    // iconPicker(),
-     inlineSvgInput(),
-    vercelDeployTool(),
-    deskTool(), visionTool() ,  media()],
-
-  schema: {
-    types: schemaTypes,
-  },
-  studio: {
-    components: {
-      navbar: StudioNavbar
-    }
-  },
-  theme: myTheme
-,
-
-  form: {
-    image: {
-      assetSources: (previousAssetSources) => {
-        return previousAssetSources.filter((assetSource) => assetSource.title !== mediaAssetSource.title);
-      },
-    },
-  },
-
-
+    structureTool(),
+    // Vision is a tool that lets you query your content with GROQ in the studio
+    // https://www.sanity.io/docs/the-vision-plugin
+    visionTool({defaultApiVersion: apiVersion}),
+  ],
 })
