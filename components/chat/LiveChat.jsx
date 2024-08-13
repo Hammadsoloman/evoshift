0fa4f906b497b3efe7490da19374cdac3dc16e66
Salim Merazga <merazgasalim@hotmail.fr>
2024-08-13 21:59:09 +0100

    fix-deployment

---
+++ components/chat/LiveChat.jsx
@@ -0,0 +1,31 @@
+"use client";
+import { useEffect } from "react";
+
+export default function LiveChat() {
+  useEffect(() => {
+    // Add Chatwoot Settings
+    window.chatwootSettings = {
+      hideMessageBubble: false,
+      position: "left", // This can be left or right
+      locale: "en", // Language to be set
+      type: "standard", // [standard, expanded_bubble]
+    };
+
+    // Paste the script from inbox settings except the <script> tag
+    (function(d,t) {
+      var BASE_URL="https://support.evoshift.net";
+      var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+      g.src=BASE_URL+"/packs/js/sdk.js";
+      g.defer = true;
+      g.async = true;
+      s.parentNode.insertBefore(g,s);
+      g.onload=function(){
+        window.chatwootSDK.run({
+          websiteToken: 'RQ7c6jFvqyhr42GWLxGsiXqK',
+          baseUrl: BASE_URL
+        })
+      }
+    })(document,"script");
+  }, []);
+  return <></>;
+}
